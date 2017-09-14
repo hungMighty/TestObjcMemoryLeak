cc322258dd5683515127084f57d44f629aac98c3
Hung Nguyen <hung.mighty.nguyen@gmail.com>
2017-09-14 09:59:09 +0700

    Test mem leak with dispatch background and on main queue

--- Test Memory Leak/Controllers/MemoryLeakViewController.m
+++ Test Memory Leak/Controllers/MemoryLeakViewController.m
@@ -32,29 +32,47 @@
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
     
-    __weak typeof(self) weakSelf = self;
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.1 * NSEC_PER_SEC), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        if (weakSelf) {
-            __strong typeof(weakSelf) strongSelf = weakSelf;
-            NSLog(@"Print out NUM: %ld", [strongSelf.aNum integerValue]);
-        }
-    });
+//    __weak typeof(self) weakSelf = self;
+//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)),
+//                   dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+//                       if (weakSelf) {
+//                           __strong typeof(weakSelf) strongSelf = weakSelf;
+//                           NSLog(@"Print out NUM after 1 second: %ld", [strongSelf.aNum integerValue]);
+//                       }
+//                   });
+//
+//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(100 * NSEC_PER_MSEC)),
+//                   dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+//                       if (weakSelf) {
+//                           __strong typeof(weakSelf) strongSelf = weakSelf;
+//                           NSLog(@"Print out NUM after 0.1 second: %ld", [strongSelf.aNum integerValue]);
+//                       }
+//                   });
 }
 
 - (void)viewDidDisappear:(BOOL)animated {
     [super viewDidDisappear:animated];
     
     __weak typeof(self) weakSelf = self;
+    
+//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+//        NSLog(@"Print out NUM - Main Queue: %ld", [self.aNum integerValue]);
+//    });
+    
+    // WILL NEVER ENTER - dealloc already called by then so weakSelf of course is nil
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         if (weakSelf) {
             __strong typeof(weakSelf) strongSelf = weakSelf;
-            NSLog(@"Print out NUM: %ld", [strongSelf.aNum integerValue]);
+            NSLog(@"Print out NUM: %ld - after 2 seconds", [strongSelf.aNum integerValue]);
         }
     });
     
-//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-//        NSLog(@"Print out NUM: %ld", [self.aNum integerValue]);
-//    });
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(100 * NSEC_PER_MSEC)), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        if (weakSelf) {
+            __strong typeof(weakSelf) strongSelf = weakSelf;
+            NSLog(@"Print out NUM - after 0.1 seconds: %ld", [strongSelf.aNum integerValue]);
+        }
+    });
 }
 
 - (void)didReceiveMemoryWarning {
